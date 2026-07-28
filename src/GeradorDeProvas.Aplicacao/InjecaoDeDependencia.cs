7c8bacbba825e3420861f92db77633ae878c191d
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-07-28 14:53:36 -0300

    Adiciona projeto inicial

---
+++ src/GeradorDeProvas.Aplicacao/InjecaoDeDependencia.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using GeradorDeProvas.Aplicacao.Modulos.ModuloDisciplina;
+using GeradorDeProvas.Aplicacao.Modulos.ModuloMateria;
+using GeradorDeProvas.Aplicacao.Modulos.ModuloQuestao;
+
+namespace GeradorDeProvas.Aplicacao;
+
+public static class InjecaoDeDependencia
+{
+    public static void AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddScoped<ServicoDisciplina>();
+        services.AddScoped<ServicoMateria>();
+        services.AddScoped<ServicoQuestao>();
+    }
+}
